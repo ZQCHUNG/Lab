20fe4cec7bec866dee2753e3a7fbf4105ca9d63e
joe <joe@changingtec.com>
2019-03-16 17:39:18 +0800

    支援比較多筆

--- CSharpAdvanceDesignTests/JoeyOrderByTests.cs
+++ CSharpAdvanceDesignTests/JoeyOrderByTests.cs
@@ -16,12 +16,36 @@ namespace CSharpAdvanceDesignTests
             this.KeyComparer = KeyComparer;
         }
 
-        private Func<Employee, string> KeySelector { get;  set; }
-        private Comparer<string> KeyComparer { get;  set; }
+        private Func<Employee, string> KeySelector { get; set; }
+        private Comparer<string> KeyComparer { get; set; }
 
         public int Compare(Employee element, Employee minElement)
         {
             return KeyComparer.Compare(KeySelector(element), KeySelector(minElement));
+        }
+    }
+
+    public class ComboComparer : IComparer<Employee>
+    {
+        public ComboComparer(IComparer<Employee> FirstCombineKeyComparer, IComparer<Employee> SecondCombineKeyComparer)
+        {
+            this.FirstCombineKeyComparer = FirstCombineKeyComparer;
+            this.SecondCombineKeyComparer = SecondCombineKeyComparer;
+        }
+
+        public IComparer<Employee> FirstCombineKeyComparer { get; private set; }
+        public IComparer<Employee> SecondCombineKeyComparer { get; private set; }
+
+        public int Compare(Employee x, Employee y)
+        {
+            var FirstCompare = FirstCombineKeyComparer.Compare(x, y);
+
+            if (FirstCompare == 0)
+            {
+                return SecondCombineKeyComparer.Compare(x, y);
+            }
+
+            return FirstCompare;
         }
     }
 
@@ -39,13 +63,12 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "Joseph", LastName = "Chen"},
                 new Employee {FirstName = "Joey", LastName = "Chen"},
             };
-            
+
             var firstkeyComparer = new CombineKeyComparer(element => element.LastName, Comparer<string>.Default);
             var secondkeyComparer = new CombineKeyComparer(element => element.FirstName, Comparer<string>.Default);
 
             var actual = JoeyOrderByLastNameAndFirstName(employees,
-                firstkeyComparer,
-                secondkeyComparer);
+                new ComboComparer(firstkeyComparer, secondkeyComparer));
 
             var expected = new[]
             {
@@ -58,9 +81,8 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
-        private IEnumerable<Employee> JoeyOrderByLastNameAndFirstName(IEnumerable<Employee> employees,
-                IComparer<Employee> FirstCombineKeyComparer,
-                IComparer<Employee> SecondCombineKeyComparer)
+        private IEnumerable<Employee> JoeyOrderByLastNameAndFirstName(IEnumerable<Employee> employees, 
+                    IComparer<Employee> comboComparer)
         {
             //bubble sort
             var elements = employees.ToList();
@@ -72,7 +94,7 @@ namespace CSharpAdvanceDesignTests
                 {
                     var element = elements[i];
 
-                    var firstCompareResult = FirstCombineKeyComparer.Compare(element, minElement);
+                    var firstCompareResult = comboComparer.Compare(element, minElement);
                     if (firstCompareResult < 0)
                     {
                         minElement = element;
@@ -80,7 +102,7 @@ namespace CSharpAdvanceDesignTests
                     }
                     else if (firstCompareResult == 0)
                     {
-                        var secondCompareResult = SecondCombineKeyComparer.Compare(element,minElement);
+                        var secondCompareResult = comboComparer.Compare(element, minElement);
                         if (secondCompareResult < 0)
                         {
                             minElement = element;
