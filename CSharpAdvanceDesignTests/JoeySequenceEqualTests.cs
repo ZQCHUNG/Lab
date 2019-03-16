75e842f59d49bc7c708a8ebe58bcd4b6da322e25
joe <joe@changingtec.com>
2019-03-16 14:45:07 +0800

    將比較物件是否相同，改成泛型，尚未完成

--- CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
+++ CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using Lab.Entities;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -63,14 +64,49 @@ namespace CSharpAdvanceDesignTests
             Assert.IsTrue(actual);
         }
 
-        private bool JoeySequenceEqual(IEnumerable<int> first, IEnumerable<int> second)
+        [Test]
+        public void two_employees_sequence_equal()
+        {
+            var first = new List<Employee>
+            {
+                new Employee() {FirstName = "Joey", LastName = "Chen", Phone = "123"},
+                new Employee() {FirstName = "Tom", LastName = "Li", Phone = "456"},
+                new Employee() {FirstName = "David", LastName = "Wang", Phone = "789"},
+            };
+
+
+            var second = new List<Employee>
+            {
+                new Employee() {FirstName = "Joey", LastName = "Chen", Phone = "123"},
+                new Employee() {FirstName = "Tom", LastName = "Li", Phone = "456"},
+                new Employee() {FirstName = "David", LastName = "Wang", Phone = "789"},
+            };
+
+
+            //IEqualityComparer<Employee> equalityComparer = new JoeyEmployeeWithPhoneEqualityComparer();
+
+            var actual = JoeySequenceEqual(
+                first,
+                second,
+                new JoeyEmployeeWithPhoneEqualityComparer());
+
+            Assert.IsTrue(actual);
+        }
+
+        private bool JoeySequenceEqual<TFirst, TSecond>(IEnumerable<TFirst> first, IEnumerable<TSecond> second)
+        {
+            return JoeySequenceEqual(first, second, EqualityComparer<TFirst>.Default);
+        }
+
+
+        private bool JoeySequenceEqual<TFirst, TSecond>(IEnumerable<TFirst> first, IEnumerable<TSecond> second, IEqualityComparer<TFirst> ECompare)
         {
             bool res = true;
 
             var firstEnumerator = first.GetEnumerator();
 
             var secondEnumerator = second.GetEnumerator();
-            
+
             while (firstEnumerator.MoveNext())
             {
                 if (!secondEnumerator.MoveNext())
@@ -78,10 +114,14 @@ namespace CSharpAdvanceDesignTests
                     return false;
                 }
 
-                if (firstEnumerator.Current != secondEnumerator.Current)
-                {
-                    return false;
-                }
+                var firstCurrent = firstEnumerator.Current;
+                var secondCurrent = secondEnumerator.Current;
+                
+
+                //if (!ECompare.Equals(firstCurrent, secondCurrent))
+                //{
+                //    return false;
+                //}
             }
 
             if (secondEnumerator.MoveNext())
--- Lab/Entities/Employee.cs
+++ Lab/Entities/Employee.cs
@@ -1,9 +1,27 @@
-﻿namespace Lab.Entities
+﻿using System.Collections.Generic;
+
+namespace Lab.Entities
 {
     public class Employee
     {
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public Role Role { get; set; }
+        public string Phone { get; set; }
+    }
+
+    public class JoeyEmployeeWithPhoneEqualityComparer : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee x, Employee y)
+        {
+            return   x.FirstName == y.FirstName
+                  && x.LastName == y.LastName
+                  && x.Phone == y.Phone;
+        }
+
+        public int GetHashCode(Employee obj)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
