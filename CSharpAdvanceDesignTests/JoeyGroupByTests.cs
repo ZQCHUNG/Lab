ddb1f065bd2f1c427d5dece02c5c1e0cc07450bf
joe <joe@changingtec.com>
2019-03-17 17:27:18 +0800

    GroupBy

--- CSharpAdvanceDesignTests/JoeyGroupByTests.cs
+++ CSharpAdvanceDesignTests/JoeyGroupByTests.cs
@@ -2,13 +2,14 @@
 using Lab.Entities;
 using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
+    //[Ignore("not yet")]
     public class JoeyGroupByTests
     {
         [Test]
@@ -23,7 +24,8 @@ namespace CSharpAdvanceDesignTests
                 new Employee {FirstName = "David", LastName = "Lee"},
             };
 
-            var actual = JoeyGroupBy(employees);
+            var actual = JoeyGroupBy(employees, 
+                employee => employee.LastName);
             Assert.AreEqual(2, actual.Count());
             var firstGroup = new List<Employee>
             {
@@ -35,9 +37,64 @@ namespace CSharpAdvanceDesignTests
             firstGroup.ToExpectedObject().ShouldMatch(actual.First().ToList());
         }
 
-        private IEnumerable<IGrouping<string, Employee>> JoeyGroupBy(IEnumerable<Employee> employees)
+        private IEnumerable<IGrouping<string, Employee>> JoeyGroupBy(IEnumerable<Employee> employees, Func<Employee, string> KeySelectror)
         {
-            throw new NotImplementedException();
+            var employeeEnumerator = employees.GetEnumerator();
+
+            var dictionary = new Dictionary<string, List<Employee>>();
+
+            while (employeeEnumerator.MoveNext())
+            {
+                var employee = employeeEnumerator.Current;
+
+                if (dictionary.ContainsKey(KeySelectror(employee)))
+                {
+                    dictionary[employee.LastName].Add(employee);
+                }
+                else
+                {
+                    dictionary.Add(employee.LastName,new List<Employee>(){employee});
+                }
+            }
+
+            return ConvertMultiGrouping(dictionary);
+        }
+
+        private IEnumerable<IGrouping<string, Employee>> ConvertMultiGrouping(Dictionary<string, List<Employee>> dictionary)
+        {
+            var dictEnumerator = dictionary.GetEnumerator();
+            
+            while (dictEnumerator.MoveNext())
+            {
+                var current = dictEnumerator.Current;
+
+                yield return new Grouping(current.Key, current.Value);
+            }
+
+        }
+    }
+
+    internal class Grouping : IGrouping<string, Employee>
+    {
+        private string key;
+        private List<Employee> value;
+
+        public Grouping(string key, List<Employee> value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+        public string Key { get; }
+
+        public IEnumerator<Employee> GetEnumerator()
+        {
+            return value.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
